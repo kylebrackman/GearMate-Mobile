f29cd0265d6ac971bf0688f4eccbdfe5d4206006
Kyle Brackman <kyle.a.brackman@gmail.com>
2024-11-08 23:06:00 -0700

    Add state to searchbar.

--- src/components/search/SearchBarCustom.tsx
+++ src/components/search/SearchBarCustom.tsx
@@ -1,13 +1,17 @@
 import { SearchBar } from '@rneui/themed';
 import { View, FlatList, StyleSheet, Text } from 'react-native';
+import { useState } from "react"
 
 const SearchBarCustom = () => {
+    const [name, setName] = useState("")
 
     return (
         <SearchBar
             lightTheme
             placeholder="Find your Gear"
             containerStyle={styles.searchBar}
+            onChangeText={setName}
+            value={name}
         />
     )
 }
