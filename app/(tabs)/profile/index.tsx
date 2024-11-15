9c98eb77aa21a83c17b95cf5218afd6e2df67e72
Kyle Trocki <ktrocki25@gmail.com>
2024-11-15 08:35:16 -0500

    Cleaned up the merge

--- app/(tabs)/profile/index.tsx
+++ app/(tabs)/profile/index.tsx
@@ -1,8 +1,12 @@
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native'
 import {useAuthService} from "@/hooks/useAuthService";
+import {globalStyles} from "@/theme/styles";
+import {AntDesign} from "@expo/vector-icons";
+import {Avatar, Card, Divider} from "@rneui/themed";
+import {router} from "expo-router";
 
 export default function ProfileScreen() {
-    const { user, signOut } = useAuthService();
+    const {user, signOut} = useAuthService();
 
     const handleLogout = async () => {
         try {
@@ -13,12 +17,30 @@ export default function ProfileScreen() {
     };
 
     return (
-        <View style={styles.container}>
-            <View style={styles.header}>
-                <Text style={styles.headerText}>Profile</Text>
-                <Text style={styles.email}>{user?.email}</Text>
+        <View style={globalStyles.marginTop20}>
+            <View style={globalStyles.profileRow}>
+                <Text style={globalStyles.header}>
+                    Profile
+                </Text>
+                <AntDesign name="bells" size={24} color="black"/>
             </View>
-
+            <View style={globalStyles.profileRow}>
+                <Avatar size={50} title="GM" containerStyle={{backgroundColor: 'black'}} rounded/>
+                <TouchableOpacity onPress={() => router.push(`/profile/details/${user?.uid}`)}>
+                    <AntDesign name="arrowright" size={24} color="black"/>
+                </TouchableOpacity>
+            </View>
+            <Divider/>
+            <Card containerStyle={globalStyles.profileCard}>
+                <Card.Title>List Your Gear</Card.Title>
+                <Card.Divider/>
+                <Text style={{textAlign: 'center'}}> Passively earn cash off your extra gear! </Text>
+            </Card>
+            <View style={globalStyles.settingsView}>
+                <Text style={[globalStyles.headerSecondary, globalStyles.bottomMargin10]}>
+                    Settings
+                </Text>
+            </View>
             <TouchableOpacity
                 style={styles.logoutButton}
                 onPress={handleLogout}
