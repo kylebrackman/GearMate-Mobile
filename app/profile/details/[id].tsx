42a77368d24edfd85ce9868f49db1d9db7c25f15
Kyle Brackman <kyle.a.brackman@gmail.com>
2024-11-14 22:14:38 -0700

    Draft profile details.

--- app/profile/details/[id].tsx
+++ app/profile/details/[id].tsx
@@ -1,11 +1,23 @@
-import { View, Text } from 'react-native'
+import {View, Text} from 'react-native'
+import {globalStyles} from '@/theme/styles';
+import {Avatar, Card, Divider} from "@rneui/themed";
 
-const id = () => {
+const profileDetails = () => {
     return (
-        <View>
-            <Text>profileDetails</Text>
+        <View style={globalStyles.marginTop20}>
+            <Divider/>
+            <Card containerStyle={globalStyles.profileCard}>
+                <Avatar size={100} title="GM" containerStyle={{backgroundColor: 'black'}} rounded/>
+                <Text style={globalStyles.headerSecondary}>Name </Text>
+                <Text>City, State</Text>
+            </Card>
+            <View style={globalStyles.settingsView}>
+                <Text style={[globalStyles.headerSecondary, globalStyles.bottomMargin10]}>
+                    Settings
+                </Text>
+            </View>
         </View>
     )
 }
 
-export default id
+export default profileDetails
