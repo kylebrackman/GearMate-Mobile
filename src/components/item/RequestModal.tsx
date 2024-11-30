8a827b462029b946a80065383a926e4f05122e69
Kyle Brackman <kyle.a.brackman@gmail.com>
2024-11-30 16:50:13 -0700

    Switch to react-native-ui-datepicker.

--- src/components/item/RequestModal.tsx
+++ src/components/item/RequestModal.tsx
@@ -1,14 +1,15 @@
 import React, {useState} from "react";
 import {View, Text, TouchableOpacity, StyleSheet} from "react-native";
 import {globalStyles} from "@/theme/styles";
-import DatePicker from "react-native-date-picker";
+import DateTimePicker from 'react-native-ui-datepicker';
+import dayjs from 'dayjs';
 
 interface RequestModalProps {
     toggleModal: () => void;
 }
 
 const RequestModal: React.FC<RequestModalProps> = ({toggleModal}) => {
-    const [date, setDate] = useState(new Date())
+    const [date, setDate] = useState(dayjs());
     const [open, setOpen] = useState(false)
 
     return (
@@ -17,11 +18,13 @@ const RequestModal: React.FC<RequestModalProps> = ({toggleModal}) => {
                 <Text>Back</Text>
             </TouchableOpacity>
             <Text>RequestModal</Text>
-            <DatePicker
-                mode="date"
-                open={open}
-                date={date}
-            />
+            <View style={styles.dateContainer}>
+                <DateTimePicker
+                    mode="range"
+                    date={date}
+                    onChange={(params) => setDate(params.date)}
+                />
+            </View>
         </View>
     )
 }
@@ -30,6 +33,10 @@ export default RequestModal
 
 const styles = StyleSheet.create({
     padding20: {
+        padding: 20
+    },
+    dateContainer: {
+        marginBottom: 20,
         padding: 20
     }
 })
