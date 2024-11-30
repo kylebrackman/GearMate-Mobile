eeaa53dcb465de975f056fa70d5bfe5cf0cc1d55
Kyle Brackman <kyle.a.brackman@gmail.com>
2024-11-30 16:38:13 -0700

    Test before deleting and restoring Pods directory.

--- src/components/item/RequestModal.tsx
+++ src/components/item/RequestModal.tsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React, {useState} from "react";
 import {View, Text, TouchableOpacity, StyleSheet} from "react-native";
 import {globalStyles} from "@/theme/styles";
+import DatePicker from "react-native-date-picker";
 
 interface RequestModalProps {
     toggleModal: () => void;
 }
 
 const RequestModal: React.FC<RequestModalProps> = ({toggleModal}) => {
+    const [date, setDate] = useState(new Date())
+    const [open, setOpen] = useState(false)
 
     return (
         <View style={[globalStyles.container, styles.padding20]}>
@@ -14,6 +17,11 @@ const RequestModal: React.FC<RequestModalProps> = ({toggleModal}) => {
                 <Text>Back</Text>
             </TouchableOpacity>
             <Text>RequestModal</Text>
+            <DatePicker
+                mode="date"
+                open={open}
+                date={date}
+            />
         </View>
     )
 }
