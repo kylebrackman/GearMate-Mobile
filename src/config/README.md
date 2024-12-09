6786f901d14e02dee110b0669480b4a1baa1bc15
Kyle Trocki <ktrocki25@gmail.com>
2024-12-08 22:08:21 -0500

    saving for now

---
+++ src/config/README.md
@@ -0,0 +1,5 @@
+
+This is what I used for google sign in
+https://www.youtube.com/watch?v=XB_gNDoOhjY
+
+
--- src/config/firebaseConfig.ts
+++ src/config/firebaseConfig.ts
@@ -21,6 +21,7 @@ const auth = initializeAuth(app, {
 
 GoogleSignin.configure({
     webClientId: process.env.EXPO_PUBLIC_GOOGLE_WEB_CLIENT_ID,
+    iosClientId: process.env.EXPO_PUBLIC_GOOGLE_IOS_CLIENT_ID,
 });
 export {auth};
 
