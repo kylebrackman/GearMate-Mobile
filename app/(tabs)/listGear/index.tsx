0b5ba2ab97c5d96a2375da726b0c8b356f032dfe
Kyle Brackman <kyle.a.brackman@gmail.com>
2024-12-21 20:57:11 -0700

    Add location dropdown to list item page.

--- app/(tabs)/listGear/index.tsx
+++ app/(tabs)/listGear/index.tsx
@@ -1,5 +1,15 @@
 import React, {useState} from 'react';
-import {View, Text, TextInput, Button, StyleSheet, ScrollView, Image, TouchableOpacity} from 'react-native';
+import {
+    View,
+    Text,
+    TextInput,
+    Button,
+    StyleSheet,
+    ScrollView,
+    Image,
+    TouchableOpacity,
+    SafeAreaView
+} from 'react-native';
 import {globalStyles, colors} from "@/theme/styles";
 import RNPickerSelect from 'react-native-picker-select';
 import * as ImagePicker from 'expo-image-picker';
@@ -37,13 +47,20 @@ const ListItemForm = () => {
         "Heavy Use"
     ];
 
+    // Make call to backend to get the locations associated with the logged in user
+    const locations = [
+        "Boulder, CO",
+        "Denver, CO",
+        "Aspen, CO"
+    ]
+
     // Best way to consolidate this function into one place? ie: also used exactly the same in login page
     const toggleModal = () => {
         setIsModalVisible(!isModalVisible);
     }
 
     const getLocation = async () => {
-        let { status } = await Location.requestForegroundPermissionsAsync();
+        let {status} = await Location.requestForegroundPermissionsAsync();
         if (status !== 'granted') {
             setErrorMsg('Permission to access location was denied');
             return;
@@ -71,94 +88,107 @@ const ListItemForm = () => {
     };
 
     return (
-        <ScrollView style={styles.scrollView}>
-            <View style={styles.container}>
-                <Text style={styles.title}>List Your Item</Text>
+        <SafeAreaView style={{flex: 1}}>
+            <ScrollView style={styles.scrollView} showsVerticalScrollIndicator={false}>
+                <View style={styles.container}>
+                    <Text style={styles.title}>List Your Item</Text>
 
-                <TextInput
-                    style={globalStyles.standardInput}
-                    placeholder="Item Name *"
-                    value={itemName}
-                    onChangeText={setItemName}
-                />
+                    <TextInput
+                        style={globalStyles.standardInput}
+                        placeholder="Item Name *"
+                        value={itemName}
+                        onChangeText={setItemName}
+                    />
 
-                <TextInput
-                    style={globalStyles.standardInput}
-                    placeholder="Price *"
-                    value={price}
-                    onChangeText={setPrice}
-                    keyboardType="numeric"
-                />
+                    <TextInput
+                        style={globalStyles.standardInput}
+                        placeholder="Price *"
+                        value={price}
+                        onChangeText={setPrice}
+                        keyboardType="numeric"
+                    />
 
-                <TextInput
-                    style={[globalStyles.standardInput, styles.multilineInput]}
-                    placeholder="Description *"
-                    value={description}
-                    onChangeText={setDescription}
-                    multiline={true}
-                    numberOfLines={4}
-                    textAlignVertical="top"
-                />
-                <View style={styles.pickerRow}>
-                    <View style={styles.pickerContainer}>
-                        <Text style={styles.pickerLabel}>Type *</Text>
-                        <RNPickerSelect
-                            onValueChange={(value) => setItemType(value)}
-                            items={
-                                itemTypes.map((type) => ({
+                    <TextInput
+                        style={[globalStyles.standardInput, styles.multilineInput]}
+                        placeholder="Description *"
+                        value={description}
+                        onChangeText={setDescription}
+                        multiline={true}
+                        numberOfLines={4}
+                        textAlignVertical="top"
+                    />
+                    <View style={styles.pickerRow}>
+                        <View style={styles.pickerContainer}>
+                            <Text style={styles.pickerLabel}>Type *</Text>
+                            <RNPickerSelect
+                                onValueChange={(value) => setItemType(value)}
+                                items={
+                                    itemTypes.map((type) => ({
+                                        label: type,
+                                        value: type,
+                                    }))
+                                }
+                            />
+                        </View>
+
+                        <View style={styles.pickerContainer}>
+                            <Text style={styles.pickerLabel}>Condition *</Text>
+                            <RNPickerSelect
+                                onValueChange={(value) => setItemCondition(value)}
+                                items={
+                                    conditions.map((condition) => ({
+                                        label: condition,
+                                        value: condition,
+                                    }))
+                                }
+                            />
+                        </View>
+                    </View>
+
+
+                    <View style={styles.center}>
+                        <TouchableOpacity onPress={pickImage} style={[styles.uploadButton, styles.width50]}>
+                            <Text style={globalStyles.buttonText}>Upload Image</Text>
+                        </TouchableOpacity>
+                        {image && <Image source={{uri: image}} style={{width: 100, height: 100, marginTop: 10}}/>}
+                    </View>
+                    <View style={[styles.center, styles.locationRow]}>
+                        <View style={styles.pickerContainer}>
+                            <Text style={styles.pickerLabel}>Select a Location</Text>
+                            <RNPickerSelect
+                                onValueChange={(value) => setItemType(value)}
+                                items={locations.map((type) => ({
                                     label: type,
                                     value: type,
-                                }))
-                            }
-                        />
+                                }))}
+                            />
+                        </View>
+                        <Text style={{marginHorizontal: 10}}>or</Text>
+                        <TouchableOpacity onPress={getLocation} style={styles.uploadButton}>
+                            <Text style={styles.uploadButtonText}>New Location</Text>
+                        </TouchableOpacity>
+                        {image && <Image source={{uri: image}} style={styles.imagePreview}/>}
                     </View>
 
-                    <View style={styles.pickerContainer}>
-                        <Text style={styles.pickerLabel}>Condition *</Text>
-                        <RNPickerSelect
-                            onValueChange={(value) => setItemCondition(value)}
-                            items={
-                                conditions.map((condition) => ({
-                                    label: condition,
-                                    value: condition,
-                                }))
-                            }
-                        />
-                    </View>
+                    <MapView
+                        style={styles.map}
+                        initialRegion={{
+                            latitude: 27.9881,
+                            longitude: 86.9250,
+                            latitudeDelta: 0.0922,
+                            longitudeDelta: 0.0421,
+                        }}
+                    />
                 </View>
-
-
-                <View style={styles.center}>
-                    <TouchableOpacity onPress={pickImage} style={styles.uploadButton}>
-                        <Text style={globalStyles.buttonText}>Upload Image</Text>
-                    </TouchableOpacity>
-                    {image && <Image source={{ uri: image }} style={{ width: 100, height: 100, marginTop: 10 }} />}
-                </View>
-                <View style={styles.center}>
-                    <TouchableOpacity onPress={getLocation} style={styles.uploadButton}>
-                        <Text style={globalStyles.buttonText}>Get Location</Text>
-                    </TouchableOpacity>
-                    {image && <Image source={{ uri: image }} style={{ width: 100, height: 100, marginTop: 10 }} />}
-                </View>
-                <MapView
-                    style={styles.map}
-                    initialRegion={{
-                        latitude: 27.9881,
-                        longitude: 86.9250,
-                        latitudeDelta: 0.0922,
-                        longitudeDelta: 0.0421,
-                    }}
-                />
-            </View>
-        </ScrollView>
+            </ScrollView>
+        </SafeAreaView>
     );
 };
 
 const styles = StyleSheet.create({
     scrollView: {
-        flex: 1,
         backgroundColor: '#fff',
-        paddingTop: 50
+        paddingTop: 50,
     },
     title: {
         fontSize: 20,
@@ -171,22 +201,6 @@ const styles = StyleSheet.create({
         paddingTop: 10,
         textAlignVertical: 'top',
     },
-    pickerContainer: {
-        marginBottom: 15,
-
-    },
-    pickerLabel: {
-        fontSize: 18,           // Slightly larger font size for emphasis
-        fontWeight: 'bold',     // Bold text for better visibility
-        color: colors.primary,  // Use the primary color for consistency
-        marginBottom: 8,        // Space between the label and the picker
-        letterSpacing: 0.5,     // Add slight spacing between letters
-    },
-    picker: {
-        backgroundColor: '#f5f5f5',
-        borderRadius: 5,
-        marginBottom: 10,
-    },
     pickerRow: {
         flexDirection: 'row',
         alignItems: 'center',
@@ -196,10 +210,11 @@ const styles = StyleSheet.create({
     map: {
         width: '100%',
         height: 200,
-        borderRadius: 10
+        borderRadius: 10,
+        paddingBottom: 10,
+        marginBottom: 60 // Increase bottom margin to ensure map is fully visible
     },
     container: {
-        flex: 1,
         justifyContent: 'center',
         paddingHorizontal: 30,
     },
@@ -210,13 +225,48 @@ const styles = StyleSheet.create({
         borderRadius: 8,
         alignItems: 'center',
         marginTop: 20,
-        width: "50%",
+        width: "40%",
         marginBottom: 10
     },
     center: {
-        flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
+    },
+    locationRow: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginBottom: 10
+    },
+    pickerContainer: {
+        flex: 1,
+        marginRight: 10,
+    },
+    pickerLabel: {
+        fontSize: 16,
+        fontWeight: '500',
+        color: colors.primary,
+        marginBottom: 5,
+    },
+    picker: {
+        backgroundColor: '#f5f5f5',
+        borderRadius: 5,
+        marginBottom: 10,
+        padding: 10,
+    },
+
+    uploadButtonText: {
+        color: '#fff',
+        fontSize: 14,
+        fontWeight: 'bold',
+    },
+    imagePreview: {
+        width: 100,
+        height: 100,
+        marginTop: 10,
+        borderRadius: 8,
+    },
+    width50: {
+        width: "50%"
     }
 });
 
