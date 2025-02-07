17da869ca6f0281cc01557c71f08aa62a976c6bd
Kyle Brackman <kyle.a.brackman@gmail.com>
2025-02-06 21:07:53 -0700

    Create api.config.ts to point towards localhost 3000, fix type definitions to match API response, and remove duplicate item types.

--- app/(tabs)/explore/index.tsx
+++ app/(tabs)/explore/index.tsx
@@ -1,69 +1,17 @@
-import React, { useState, useEffect } from 'react';
-import { View, FlatList } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {View, FlatList} from 'react-native';
 import ItemCard from '@/src/components/item/ItemCard';
 import SearchBarCustom from '@/src/components/search/SearchBarCustom';
 import ItemTypeSearch from '@/src/components/search/ItemTypeSearch';
-import { useRouter } from "expo-router";
+import {useRouter} from "expo-router";
 import {globalStyles} from "@/theme/styles";
 import {StyleSheet} from "react-native";
 import {getAllItemsApi} from "@/services/apis/ItemApi";
-
-type Item = {
-    id: number;
-    name: string;
-    image: any;
-    description: string;
-    price: number;
-    condition: string;
-};
-
-const allItems: Item[] = [
-    {
-        id: 1,
-        name: 'Mountain Bike',
-        image: require('../../../assets/images/item/mountain-bike.png'),
-        description: 'Great for off-road trails',
-        price: 25,
-        condition: 'New',
-    },
-    {
-        id: 2,
-        name: 'Fishing Pole',
-        image: require('../../../assets/images/item/fishing-pole.png'),
-        description: 'Perfect for your next fishing trip',
-        price: 15,
-        condition: 'Like New',
-    },
-    {
-        id: 3,
-        name: 'Kayak',
-        image: require('../../../assets/images/item/kayak.png'),
-        description: 'Enjoy a day on the water',
-        price: 30,
-        condition: 'New',
-    },
-    {
-        id: 4,
-        name: 'Snowboard Boots',
-        image: require('../../../assets/images/item/snowboard-boots.png'),
-        description: 'Stay warm and stylish on the slopes',
-        price: 20,
-        condition: 'Like New',
-    },
-    {
-        id: 5,
-        name: 'Yeti Cooler',
-        image: require('../../../assets/images/item/yeti-cooler.png'),
-        description: 'Keep your drinks cold for days',
-        price: 50,
-        condition: 'Used',
-    },
-    // Add more userItems as needed
-];
+import {Item} from "@/types/models.types";
 
 export default function ExploreScreen() {
 
-    const [itemsBeta, setAllItemsBeta] = useState([]);
+    const [itemsBeta, setItemsBeta] = useState<Item[]>([]);
     const router = useRouter();
 
     useEffect(() => {
@@ -73,14 +21,13 @@ export default function ExploreScreen() {
 
     const fetchAllItems = async () => {
         try {
-            await getAllItemsApi().then((data) => {
-                setAllItemsBeta(data);
-            });
+            const data = await getAllItemsApi();
+            setItemsBeta(data);
         } catch (error) {
             console.error('Error fetching allItems:', error);
         }
     };
-    const renderItem = ({ item }: { item: Item }) => (
+    const renderItem = ({item}: { item: Item }) => (
         <ItemCard
             id={item.id}
             name={item.name}
@@ -93,14 +40,14 @@ export default function ExploreScreen() {
 
     return (
         <View style={styles.exploreContainer}>
-            <SearchBarCustom />
-            <ItemTypeSearch />
+            <SearchBarCustom/>
+            <ItemTypeSearch/>
             <FlatList
-                data={allItems}
+                data={itemsBeta}
                 renderItem={renderItem}
                 keyExtractor={item => item.id.toString()}
                 showsVerticalScrollIndicator={false}
-                contentContainerStyle={{ paddingBottom: 50 }}
+                contentContainerStyle={{paddingBottom: 50}}
             />
         </View>
     );
--- services/apis/ItemApi.ts
+++ services/apis/ItemApi.ts
@@ -1,14 +1,17 @@
 import { Item } from "@/types/models.types";
 import { ErrorResponse } from "@/types/responses.types";
+import { API_BASE_URL } from "@/src/config/api.config";
 
 export const getAllItemsApi = async (): Promise<Item[]> => {
     try {
-        const response = await fetch('/api/items?all_items=true');
+        const response = await fetch(`${API_BASE_URL}/api/items?all_items=true`);
+        console.log(`${API_BASE_URL}/api/items?all_items=true`)
         if (!response.ok) {
             const errorData = (await response.json()) as ErrorResponse;
-            throw new Error(`${errorData.errors.join(', ')}`);
+            throw new Error(errorData.error);
         } else {
             const items: Item[] = (await response.json()) as Item[];
+            console.log(items)
             return items;
         }
     } catch (error) {
